0e9c3bd49ea0f44e8c44f70ba6b49052a8037622
Michael Bany <mobany526@gmail.com>
2025-06-27 19:59:39 +0200

    first commit

---
+++ src/Providers/RosalanaRolesServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Rosalana\Roles\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RosalanaRolesServiceProvider extends ServiceProvider
+{
+    /**
+     * Register everything in the container.
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        //
+    }
+}
