4fdd8004368f621739f495b557ea0dac634b3421
Michael Bany <mobany526@gmail.com>
2025-06-29 18:19:14 +0200

    global registry roleble

--- src/Support/Registry.php
+++ src/Support/Registry.php
@@ -4,11 +4,41 @@ namespace Rosalana\Roles\Support;
 
 class Registry
 {
-    protected static array $permissions = [];
+    protected static array $models = [];
 
-
-    public static function register(string $model, string $permissions): void
+    public static function register(string $class): void
     {
-        //
+        static::$models[$class] = [
+            'pivot_table' => $class::getUsersPivotTable(),
+            'default_roles' => $class::defaultRoles(),
+            'permissions' => $class::permissions(),
+            'alias' => $class::permissionsAlias(),
+        ];
     }
+
+    public static function all(): array
+    {
+        return static::$models;
+    }
+
+    public static function get(string $class): ?array
+    {
+        return static::$models[$class] ?? static::resolve($class);
+    }
+
+    protected static function resolve(string $class): ?array
+    {
+        if (!class_exists($class)) {
+            throw new \RuntimeException("Class {$class} not found.");
+        }
+
+        if (!in_array('Rosalana\Roles\Traits\Roleable', class_uses_recursive($class))) {
+            throw new \RuntimeException("Class {$class} does not use Roleable trait.");
+        }
+
+        static::register($class);
+        return static::$models[$class] ?? null;
+    }
+
+    // Permissions potřebují být nějak processnuté - protože mají aliasy + windcards
 }
