ccf5be169998e62bccc81cf539f8982a53e543b7
Michael Bany <mobany526@gmail.com>
2025-07-27 20:55:03 +0200

    odpovída na pipelines

--- src/Providers/RosalanaRolesServiceProvider.php
+++ src/Providers/RosalanaRolesServiceProvider.php
@@ -3,7 +3,9 @@
 namespace Rosalana\Roles\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Rosalana\Core\Facades\Basecamp;
 use Rosalana\Roles\Services\RolePolicyResolver;
+use Illuminate\Http\Client\Response;
 
 class RosalanaRolesServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,27 @@ class RosalanaRolesServiceProvider extends ServiceProvider
         $this->app->singleton('rosalana.roles', function ($app) {
             return new \Rosalana\Roles\Services\RolesManager();
         });
+
+        /** TODO -> */
+        Basecamp::after('user.login', function (Response $response) {
+            $globalRole = $response->json('data.role', null);
+            logger()->info('User logged in with role: ' . $globalRole);
+        });
+
+        Basecamp::after('user.register', function (Response $response) {
+            $globalRole = $response->json('data.role', null);
+            logger()->info('User registered with role: ' . $globalRole);
+        });
+
+        Basecamp::after('user.refresh', function (Response $response) {
+            $globalRole = $response->json('data.role', null);
+            logger()->info('User refreshed with role: ' . $globalRole);
+        });
+
+        Basecamp::after('user.current', function (Response $response) {
+            $globalRole = $response->json('data.role', null);
+            logger()->info('Current user role: ' . $globalRole);
+        });
     }
 
     /**
@@ -22,7 +45,6 @@ class RosalanaRolesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
         RolePolicyResolver::register();
 
         $this->publishes([
